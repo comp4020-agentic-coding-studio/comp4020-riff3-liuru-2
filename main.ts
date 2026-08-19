36007d8a9093b48e7a669c573c9c60abf95711fc
Ray0766 <222364120+Ray0766@users.noreply.github.com>
2026-08-19 16:37:44 +1000

    add mouse-following spring trail layer

--- main.ts
+++ main.ts
@@ -386,3 +386,8 @@ initShadow();
 initDew();
 initLightning();
 initSoundToggle();
+
+import { initTrails } from "./trails";
+
+const trailsCanvas = document.querySelector<HTMLCanvasElement>("[data-trails]");
+if (trailsCanvas) initTrails(trailsCanvas);
